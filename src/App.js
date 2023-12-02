06ec5baf76e3708d6aa0ec3024816cb6d5348db3
mariyasada <mariyasada2406@gmail.com>
2023-12-02 15:20:42 +0530

    removing some unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import UsersListPage from "./Page/UsersListPage/UsersListPage";
-import UsersPage from "./Page/UsersPage/UsersPage";
 
 function App() {
   return (
     <div className="App">
-      {/* <UsersPage /> */}
       <UsersListPage />
     </div>
   );
